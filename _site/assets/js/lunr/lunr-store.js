bd6a32c8fbe32d88a4b7b777e14961e039bd77b2
Nahuatl_19650 <asanch5169@gmail.com>
2018-12-08 13:00:29 -0500

    deleted git ignore

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,6 @@
+var store = [{
+        "title": "Test Post",
+        "excerpt":"this is a test post  ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/test-post/",
+        "teaser":null}]
