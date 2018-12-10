d9a6703ffc7e27686903e8203d0887ae2baea43f
Nahuatl_19650 <asanch5169@gmail.com>
2018-12-09 19:06:11 -0500

    updates

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,6 +1,6 @@
 var store = [{
-        "title": "Test Post",
-        "excerpt":"this is a test post  ","categories": [],
+        "title": "Does It Exist",
+        "excerpt":"Wooh! Existence. Simple enough for a simple post. While working with data in Excel, often, the question “Does this data exist within another data set?” comes up. There are several methods of accomplishing this task. My go-to was the =VLOOKUP(cell,single_column,1,0). If the value exists, it simply returns the same value...","categories": [],
         "tags": [],
-        "url": "http://localhost:4000/test-post/",
+        "url": "http://localhost:4000/does-it-exist/",
         "teaser":null}]
