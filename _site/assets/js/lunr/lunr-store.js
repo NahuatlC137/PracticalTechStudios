7413404030cfa6cfe530c0b72859313ae89f2698
Nahuatl_19650 <asanch5169@gmail.com>
2018-12-12 23:01:57 -0500

    configurations

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -3,4 +3,4 @@ var store = [{
         "excerpt":"Wooh! Existence. Simple enough. While working with data in Excel, often, we question “Does this value(s) exist within a data set?”. Below, I’ve outlined a couple of methods to answer this question. VLOOKUP or MATCH My go-to function was =VLOOKUP(cell,single_column,1,0). If the value exists, it simply returns itself, else #N/A,...","categories": [],
         "tags": [],
         "url": "http://localhost:4000/does-it-exist/",
-        "teaser":null}]
+        "teaser":"http://localhost:4000/assets/Color_Splash.jpg"}]
