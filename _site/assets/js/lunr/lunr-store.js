8b4f8f9952132724b9363c4d9f77a72a3523dc70
Nahuatl_19650 <asanch5169@gmail.com>
2018-12-14 00:07:40 -0500

    saving md

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -5,7 +5,7 @@ var store = [{
         "url": "http://localhost:4000/does-it-exist/",
         "teaser":"http://localhost:4000/assets/images/color_splash.jpg"},{
         "title": "Real Estate Venture V2",
-        "excerpt":" ","categories": [],
+        "excerpt":"Not all stories start where they need to. This is the story of my second rodeo with real estate. My projects always begin with moments of unrest. What to do? What to do..? Damn it, why the hell can’t you just stay still. I’ve learned to cherish these moments. Those...","categories": [],
         "tags": [],
         "url": "http://localhost:4000/real-estate-venture-v2/",
         "teaser":"http://localhost:4000/assets/images/color_splash.jpg"}]
